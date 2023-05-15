49c179a9afc39c75cbd90d5a6a9591a9fb6cf9cb
Harry <jeongus97@gmail.com>
2023-05-15 16:37:22 +0900

    파일정리 스크립트 (강의 내용)
    * 강의대로 따라하면서 코드 작성

--- photo/app.js
+++ photo/app.js
@@ -1,45 +1,82 @@
-// video 폴더 안에는 .mp4, .mov 파일
-// captured 폴더 안에는 .png, .aae 파일
-// duplicated 폴더 안에는 중복된 파일
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
 
-const fs = require('fs').promises;
+// 계획
+// 1. 사용자가 원하는 폴더의 이름을 받아온다.
+// 2. 그 폴더안에 video, captured, duplicated 폴더를 만든다.
+// 3. 폴더 안에 있는 파일들을 다 돌면서 해당하는 mp4/mov, png/aae, IMG_1234 (IMG_E1234)
 
-const dirName = process.argv.slice(2);
-const data = [];
-const readDir = fs.readdir(__dirname + '/' + dirName[0])
-    .then(files => {
-        saveData(files);
-        divisionData();
-    })
-    .catch(console.error);
 
-function saveData(files) {
-    files.forEach(file => {
-        data.push(file);
-    });
+// 1. 사용자가 원하는 폴더의 이름을 받아온다.
+const folder = process.argv[2];
+const workingDir = path.join(__dirname, folder);
+
+// 폴더가 존재하지 않거나 fs에 존재하지 않는 경로라면 에러를 출력
+if (!folder || !fs.existsSync(workingDir)) {
+    console.error('Please enter folder name in Pictures');
+    return;
 }
 
-function divisionData() {
-    data.forEach(file => {
-        if (file.includes('.mp4') || file.includes('.mov')) {
-            checkDirectory(file);
-        } else if (file.includes('.png') || file.includes('.aae')) {
-            console.log('captured');
-        } else if (file.includes('(1)')) {
-            console.log('duplicated');
+// 2. 그 폴더안에 video, captured, duplicated 폴더를 만든다.
+const videoDir = path.join(workingDir, 'video');
+const capturedDir = path.join(workingDir, 'captured');
+const duplicatedDir = path.join(workingDir, 'duplicated');
+
+// 폴더를 만들고 파일들을 처리해줘야하기 때문에 -> 동기적으로 처리를 해줘야하기 때문에
+!fs.existsSync(videoDir) && fs.mkdirSync(videoDir);
+!fs.existsSync(capturedDir) && fs.mkdirSync(capturedDir);
+!fs.existsSync(duplicatedDir) && fs.mkdirSync(duplicatedDir);
+
+// 3. 폴더 안에 있는 파일들을 다 돌면서 해당하는 mp4/mov, png/aae, IMG_1234 (IMG_E1234)
+fs.promises
+    .readdir(workingDir)
+    .then(processFiles)
+    .catch(console.error);
+
+function processFiles(files) {
+    files.forEach((file) => {
+        if (isVideoFile(file)) {
+            move(file, videoDir);
+        } else if (isCapturedFile(file)) {
+            move(file, capturedDir);
+        } else if (isDuplicatedFile(files, file)) {
+            move(file, duplicatedDir);
         }
     });
 }
 
-function checkDirectory(file) {
-    console.log(fs.readdir(__dirname + '/' + dirName[0] + '/video'));
-    fs.readdir(__dirname + '/' + dirName[0] + '/video')
-        .then(moveToVideo(file))
-        .catch(console.error);
+function isVideoFile(file) {
+    const regExp = /(mp4|mov)$/gm;
+    const match = file.match(regExp);
+    return !!match; // match가 null이면 false, 값이 있으면 true
 }
 
-function moveToVideo(file) {
-    fs.rename(__dirname + '/' + dirName[0] + '/' + file, __dirname + '/' + dirName[0] + '/video/' + file)
-        .then(() => console.log('move to video'))
+function isCapturedFile(file) {
+    const regExp = /(png|aae)$/gm;
+    const match = file.match(regExp);
+    return !!match;
+}
+
+function isDuplicatedFile(files, file) {
+    // IMG_XXXX -> IMG_EXXXX
+    // IMG_로 시작하지 않는다면 false 반환
+    if (!file.startsWith('IMG_') || file.startsWith('IMG_E')) {
+        return false;
+    }
+
+    // IMG_XXXX -> XXXX -> EXXXX
+    const edited = `IMG_E${file.split('_')[1]}`;
+
+    // edited가 files에 존재한다면 true 반환
+    const found = files.find((f) => f.includes(edited));
+    return !!found;
+}
+
+// 파일을 옮기는 함수
+function move(file, targetDir) {
+    console.info(`move ${file} to ${path.basename(targetDir)}`);
+    fs.promises
+        .rename(path.join(workingDir, file), path.join(targetDir, file))
         .catch(console.error);
 }
